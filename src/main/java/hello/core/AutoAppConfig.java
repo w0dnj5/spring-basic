284c918696f7d7489675d24038b82740e5febf3d
w0dnj5 <jw991211@icloud.com>
2024-12-26 18:28:53 +0900

    comment: 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // basePackages = "hello.core.member", 탐색할 패지지의 시작 위치 지정 이 패키지 포함 하위 패키지를 모두 탐색
+        // basePackageClasses = AutoAppConfig.class,
+        // 지정하지 않으면 (기본값) package를 기준으로 하위 패키지를 모두 탐색 (프로젝트 최상단으로 지정)
         // 기존 AppConfig 클래스를 살려두기 위해서 excludeFilters 적용
         // @Configuration 제외 이유 -> @ComponentScan 존재 -> 스캔 대상
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
