95bcfdba9c3a6e06b800f702697cb74e51d1947a
w0dnj5 <jw991211@icloud.com>
2024-12-27 21:16:13 +0900

    comment: 조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,14 +2,16 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+// @Component 스캔 시 하위 타입이 2개 이상일 때 오류 발생
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
 
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade() == Grade.VIP) {
+        if (member.getGrade() == Grade.VIP) {
             return discountFixAmount;
         }
         return 0;
